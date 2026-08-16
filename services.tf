043fc90b199ee0ccd350024ba9a09bc2991f69a4
Karlos Helton Braga <karlos.hbraga@gmail.com>
2026-08-16 19:23:45 +0100

    devops[terraform]: Enable required Google Cloud services

---
+++ services.tf
@@ -0,0 +1,23 @@
+locals {
+  required_services = toset([
+    "artifactregistry.googleapis.com",
+    "billingbudgets.googleapis.com",
+    "compute.googleapis.com",
+    "dns.googleapis.com",
+    "iam.googleapis.com",
+    "monitoring.googleapis.com",
+    "run.googleapis.com",
+    "secretmanager.googleapis.com",
+    "servicenetworking.googleapis.com",
+    "sqladmin.googleapis.com",
+    "vpcaccess.googleapis.com",
+  ])
+}
+
+resource "google_project_service" "required" {
+  for_each = local.required_services
+
+  project            = var.project_id
+  service            = each.value
+  disable_on_destroy = false
+}
