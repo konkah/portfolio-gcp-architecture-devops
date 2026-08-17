a8baad009235b540a40253c20df0300e1e04d1ea
Karlos Helton Braga <karlos.hbraga@gmail.com>
2026-08-17 11:51:50 +0100

    devops[terraform]: Declare random provider explicitly

--- main.tf
+++ main.tf
@@ -6,6 +6,10 @@ terraform {
       source  = "hashicorp/google"
       version = "~> 7.0"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
   }
 
   # GCS Backend Configuration for Terraform State
