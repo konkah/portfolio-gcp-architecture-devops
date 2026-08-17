b95a70c22ef252b55b2ac9bff5114c0899a66abb
Karlos Helton Braga <karlos.hbraga@gmail.com>
2026-08-17 08:04:45 +0100

    devops[terraform]: Create router and NAT for outbound internet access

---
+++ router.tf
@@ -0,0 +1,17 @@
+resource "google_compute_router" "nat" {
+  name    = "${var.env}-router"
+  network = google_compute_network.vpc.id
+  region  = var.region
+
+  depends_on = [google_compute_subnetwork.primary]
+}
+
+resource "google_compute_router_nat" "nat" {
+  name                               = "${var.env}-router-nat"
+  router                             = google_compute_router.nat.name
+  region                             = google_compute_router.nat.region
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+
+  depends_on = [google_compute_router.nat]
+}
