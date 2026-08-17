939a05ad0254b2c53edd77eb71f76279df56719a
Karlos Helton Braga <karlos.hbraga@gmail.com>
2026-08-17 07:54:45 +0100

    devops[terraform]: Create primary subnet

--- network.tf
+++ network.tf
@@ -5,3 +5,12 @@ resource "google_compute_network" "vpc" {
 
   depends_on = [google_project_service.required]
 }
+
+resource "google_compute_subnetwork" "primary" {
+  name          = "${var.env}-subnet-primary"
+  ip_cidr_range = var.subnet_cidr
+  region        = var.region
+  network       = google_compute_network.vpc.id
+
+  depends_on = [google_compute_network.vpc]
+}
--- variables.tf
+++ variables.tf
@@ -14,3 +14,9 @@ variable "env" {
   type        = string
   default     = "dev"
 }
+
+variable "subnet_cidr" {
+  description = "CIDR block for the primary subnet."
+  type        = string
+  default     = "10.0.1.0/24"
+}
